9118991292f5eaf0d3dc34aa88a61a289e52f378
akshayaj108 <akshayjadhao806@gmail.com>
2026-01-10 14:12:44 +0530

    setup database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,17 @@
 import { config } from "dotenv";
+import path from "path";
 
-config();
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USER_NAME, DB_PASS, DB_NAME } =
+  process.env;
 
 export const CONFIG = {
   PORT,
   NODE_ENV,
+  DB_HOST,
+  DB_PORT,
+  DB_USER_NAME,
+  DB_PASS,
+  DB_NAME,
 };
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,18 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import { User } from "./entity/User";
+import { CONFIG } from "./config";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "test",
-  password: "test",
-  database: "test",
-  synchronize: true,
+  host: CONFIG.DB_HOST!,
+  port: Number(CONFIG.DB_PORT),
+  username: CONFIG.DB_USER_NAME!,
+  password: CONFIG.DB_PASS!,
+  database: CONFIG.DB_NAME!,
+  //Don't use this prod
+  synchronize: CONFIG.NODE_ENV === "test" || CONFIG.NODE_ENV === "dev",
   logging: false,
   entities: [User],
   migrations: [],
   subscribers: [],
-});
+} as DataSourceOptions);
