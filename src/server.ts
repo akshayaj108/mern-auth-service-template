e9b8217d7973ba1873beaaa7cb379eafad7e1d22
akshayaj108 <akshayjadhao806@gmail.com>
2026-06-01 21:31:45 +0530

    create admin at initial launch of server

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import app from "./app";
 import { CONFIG } from "./config";
 import { AppDataSource } from "./config/data-source";
 import logger from "./config/logger";
+import { createAdmin } from "./utils";
 
 const startServer = async () => {
   try {
     await AppDataSource.initialize();
     logger.info("Database connected successfully.");
+    await createAdmin();
     app.listen(CONFIG.PORT, () => {
       logger.info(`Server started on port ${CONFIG.PORT}`);
     });
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,39 @@
-export function calculateDiscount(price: number, discountRate: number): number {
-  return price - (price * discountRate) / 100;
+import { AppDataSource } from "./config/data-source";
+import logger from "./config/logger";
+import { User } from "./entity/User";
+import bcrypt from "bcryptjs";
+import { Roles } from "./constants";
+
+export async function createAdmin() {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const adminRole = Roles.ADMIN;
+  const existingAdmin = await userRepository.findOneBy({ role: adminRole });
+
+  if (existingAdmin) {
+    console.log("Admin user already exists.");
+    return;
+  }
+
+  const adminUser = new User();
+  adminUser.firstName = process.env.ADMIN_FIRST_NAME || "System";
+  adminUser.lastName = process.env.ADMIN_LAST_NAME || "Admin";
+  adminUser.email = process.env.ADMIN_EMAIL || "admin@mern.com";
+  adminUser.pass = await getHashPassword(
+    process.env.ADMIN_PASSWORD || "password",
+  );
+  adminUser.role = adminRole;
+
+  try {
+    await userRepository.save(adminUser);
+    logger.info("Admin user created successfully.");
+  } catch (error) {
+    console.error("Error creating admin user:", error);
+    logger.info("Admin user already created by another instance.");
+  }
+}
+export async function getHashPassword(password: string) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
 }
